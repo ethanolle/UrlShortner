0fd93fb0256cea7a8b7b456af82009fd4d222783
ethan ethan <ethan.sayagh@gmail.com>
2020-06-02 07:40:14 +0300

    adding files

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const app = express();
+
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/clean", async (req, res) => {
+  ShortUrl.deleteMany({}, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+app.post("/delete/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOneAndDelete({
+    short: req.params.shortUrl,
+  });
+  if (shortUrl == null) return res.sendStatus(404);
+  res.redirect("/");
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000);
